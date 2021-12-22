e585555132dd731fe216b86ddd6bbc3ab4286d0e
Your Name <you@example.com>
2021-12-22 17:25:59 +0000

    port changed in app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 const userController = require('./controllers/userController');
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.get('/users', userController.getAllUSer);
@@ -11,7 +12,7 @@ app.patch('/users', userController.updateUSerById );
 app.delete('/users', userController.deleteUSerById);
 app.get('/users/:userId', userController.getUSerById);
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
   console.log('Server has started to run');
   mongoose.connect(process.env.DB_URL)
   .then(function(){
